495f7ed101ac297c29d7a5ce26c4c5ac580d6493
andrelohmann <lohmann.andre@gmail.com>
2015-01-13 06:26:09 +0100

    fixed proxy prblem
    now also setting of a differen $_SERVER Var is possible (HTTP_X_FROWARDED_FOR instead REMOTE_ADDR)

--- _config.php
+++ _config.php
@@ -7,6 +7,6 @@ Member::add_extension('BootstrapNavbarLanguageFormMember');
 // Add the Following Lines to your _config.php file
 //if(Member::currentUser() && Member::currentUser()->LocaleCode) i18n::set_locale(Member::currentUser()->LocaleCode);
 //elseif(Session::get('LocaleCode')) i18n::set_locale(Session::get('LocaleCode'));
-//elseif(($Locale = LocaleGeoip::countryCode2Locale(Geoip::ip2country($_SERVER['REMOTE_ADDR'], true)))) i18n::set_locale($Locale);
+//elseif(($Locale = LocaleGeoip::countryCode2Locale(Geoip::ip2country($_SERVER[Geoip::set_default_server_var()], true)))) i18n::set_locale($Locale);
 //elseif(($Locale = LocaleGeoip::countryCode2Locale(Geoip::get_default_country_code()))==true) i18n::set_locale($Locale);
 //else i18n::set_locale(LocaleGeoip::get_default_locale_code());
