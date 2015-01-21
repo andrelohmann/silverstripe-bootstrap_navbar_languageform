0379b8022beeca2e9097a121e08fa808bec5febf
andrelohmann <lohmann.andre@gmail.com>
2015-01-21 16:23:28 +0100

    Fixed an error

--- code/config/BootstrapNavbarLanguageFormController.php
+++ code/config/BootstrapNavbarLanguageFormController.php
@@ -18,7 +18,7 @@ class BootstrapNavbarLanguageFormController extends Extension {
         
         if(Member::currentUser() && Member::currentUser()->Locale) i18n::set_locale(Member::currentUser()->Locale);
         elseif(Session::get('Locale')) i18n::set_locale(Session::get('Locale'));
-        elseif(($Locale = LocaleGeoip::countryCode2Locale(Geoip::ip2country($_SERVER[Geoip::set_default_server_var()], true)))) i18n::set_locale($Locale);
+        elseif(($Locale = LocaleGeoip::countryCode2Locale(Geoip::ip2country($_SERVER[Geoip::get_default_server_var()], true)))) i18n::set_locale($Locale);
         elseif(($Locale = LocaleGeoip::countryCode2Locale(Geoip::get_default_country_code()))==true) i18n::set_locale($Locale);
         else i18n::set_locale(LocaleGeoip::get_default_locale_code());
         
